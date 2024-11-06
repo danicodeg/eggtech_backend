a9a9463e5549ac722ba23a52f6a444723eda8362
danicodeg <dasanchezgonza@gmail.com>
2024-11-05 21:23:21 -0500

    bill view retoques

--- app/api/routes/bill_view_router.py
+++ app/api/routes/bill_view_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, Body, HTTPException, status
 from app.api.schema import bill_schema
-from app.api.services.bill_view import create_Bill, get_all_bills, get_bill_view_by_id
+from app.api.services.bill_view import create_Bill, get_all_bills, get_all_bills_detail, get_bill_view_by_id, get_one_bills
 from app.api.utils.db import get_db
 
 router = APIRouter(prefix="/api")
@@ -8,7 +8,7 @@ router = APIRouter(prefix="/api")
 #bill post
 
 @router.post(
-     "/bill/",
+     "/bills/",
      tags=["create"],
      status_code=status.HTTP_201_CREATED,
      dependencies=[Depends(get_db)]
@@ -26,9 +26,28 @@ def create(bill: bill_schema.BillBase = Body(...)):
 def get_all():
     return get_all_bills()
 
-
+@router.get(
+    "/bills/{bills_id}",
+    tags=["bills"],
+    status_code=status.HTTP_200_OK,
+    dependencies=[Depends(get_db)]
+)
+def get_bill_by_id(bills_id: int):
+    bill = get_one_bills(bills_id)
+    if not bill:
+        return {"message": "Bill not found", "status_code": 404}
+    return bill
 
 # view bill
+
+@router.get(
+    "/bill-view",
+    status_code=status.HTTP_200_OK,
+    dependencies=[Depends(get_db)]
+)
+def get_all_bill_details():
+    return get_all_bills_detail()
+
 @router.get(
     "/bill-view/{bill_id}",
     tags=["bill-view"],
--- app/api/services/bill_view.py
+++ app/api/services/bill_view.py
@@ -11,7 +11,7 @@ def create_Bill(bills: bill_schema.BillBase):
     bill=Bill(
         bill_date=bills.bill_date,
         total=bills.total,
-        customers_id=bills.id_customers,
+        customers_id=bills.customers_id,
         bill_type_id=bills.bill_type_id,
         status_id =bills.status_id,
         created_at=datetime.datetime.now())
@@ -25,7 +25,10 @@ def get_all_bills():
        
     return list(Bill.select().dicts())
 
+def get_one_bills(bills_id: int):
+    bills = Bill.filter(Bill.id == bills_id).dicts().first()
 
+    return bills
 
 # este es el de detalles/vista
 def get_all_bills_detail():
